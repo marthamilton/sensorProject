8584faac962de39779b54193fd0546a9b5e205d5
Martha <martha@themiltons.co.uk>
2020-03-02 09:22:44 +0000

    Removed Cesium default features
    Added in Cesium access token and removed unnecessary default features e.g. search bar

--- js/libs/index.js
+++ js/libs/index.js
@@ -1,6 +1,62 @@
 requirejs(['cesium'], function (Cesium) {
-    
-    Cesium.Ion.defaultAccessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJjMDgxODJmNS1lNDg5LTRkOTktOWQyZi1lYWQyNDNhZWUwMzQiLCJpZCI6MjMxMjUsInNjb3BlcyI6WyJhc3IiLCJnYyJdLCJpYXQiOjE1ODI3NTk4ODN9.Gq-ahGBhm4OfLhnaEOezN1E38KhlqhBLQVfsa3aKBTA';
-    var viewer = new Cesium.Viewer('cesiumContainer');
-    viewer.infoBox.frame.sandbox = "allow-scripts";
+
+    // Default CesiumJS access token
+    Cesium.Ion.defaultAccessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiIzNGE5YjRkNy0yYzE1LTRiMTEtYmIwNC03ZjI4OTYyMTRlZTkiLCJpZCI6MjMxMTcsInNjb3BlcyI6WyJhc3IiLCJnYyJdLCJpYXQiOjE1ODI3Mzk0MzV9.kEPRva0D_vJ-mxASc7jdGAGL67M5GiT6r5sQ4LcgHwY';
+
+    // New CesiumJS Viewer
+    const viewer = new Cesium.Viewer('cesiumContainer', {
+        // Start in columbus viewer
+        sceneMode: Cesium.SceneMode.SCENE3D,
+
+        // Use Cesium World Terrain
+        terrainProvider: Cesium.createWorldTerrain(),
+
+        // Hide the base layer picker
+        baseLayerPicker: false,
+
+        // Use BingMaps - Attached to martha@themiltons.co.uk
+        bing: new Cesium.BingMapsImageryProvider({
+            url : 'https://dev.virtualearth.net',
+            key: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI3NzZkYTg0OS0zZmFhLTQxMDAtOGEwOC1lMjY3MTRjY2QwMzEiLCJpZCI6MjMxMTcsInNjb3BlcyI6WyJhc2wiLCJhc3IiLCJhc3ciLCJnYyJdLCJpYXQiOjE1ODMxMzk1OTR9.yErw5tu4nqNGtDYJQgVcBg1_2up4QGUE4Z4htNAhCks',
+            mapStyle : Cesium.BingMapsStyle.AERIAL
+        }),
+        geocoder : [
+            new Cesium.CartographicGeocoderService(),
+            new Cesium.BingMapsGeocoderService({key:'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI3NzZkYTg0OS0zZmFhLTQxMDAtOGEwOC1lMjY3MTRjY2QwMzEiLCJpZCI6MjMxMTcsInNjb3BlcyI6WyJhc2wiLCJhc3IiLCJhc3ciLCJnYyJdLCJpYXQiOjE1ODMxMzk1OTR9.yErw5tu4nqNGtDYJQgVcBg1_2up4QGUE4Z4htNAhCks'})
+        ],
+        terrainProvider: Cesium.createWorldTerrain({
+            requestWaterMask: true,
+            requestVertexNormals: true
+        }),
+
+        // Hiding the home button
+        homeButton: false,
+        // Allowing animations
+        animation: false,
+        // Disabling VR Support
+        vrButton: false,
+        // Disabling the timeline
+        timeline: false,
+        // Disabling default search box
+        geocoder: false,
+        // Disabling the 2D Mode
+        sceneModePicker: false,
+        // Disables default navigation help
+        navigationHelpButton: false,
+        // Hiding fullscreen button
+        fullscreenButton: false,
+        // Show Columbus View map with Web Mercator projection
+        mapProjection: new Cesium.WebMercatorProjection()
+    });
+
+    // Disables locking when double clicking a point on the map
+    viewer.cesiumWidget.screenSpaceEventHandler.removeInputAction(Cesium.ScreenSpaceEventType.LEFT_DOUBLE_CLICK);
+
+    // Zoom to UK immediately
+    const center = Cesium.Cartesian3.fromDegrees(-1.43921, 52.99495, 2000000.0);
+    viewer.camera.flyTo({
+        destination: center,
+        duration: 2
+    });             
 });
+

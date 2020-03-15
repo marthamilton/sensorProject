bae6903d5879b4bf587fe0499bc73d30e3c21895
Martha <martha@themiltons.co.uk>
2020-03-15 17:07:33 +0000

    updated menu

--- js/libs/Cesium/ThirdParty/rConf.js
+++ js/libs/Cesium/ThirdParty/rConf.js
@@ -4,8 +4,6 @@ requirejs.config({
         jquery: 'jquery-3.4.1.min',
         cesium: 'Cesium/Cesium',
         main: 'index',
-        displayManager: 'displayManager',
-        aboutBoxContent: 'aboutBoxContent'
     }
 });
 requirejs(['jquery'], function ($) {
@@ -15,11 +13,5 @@ requirejs(['cesium'], function ($) {
     console.log($) // OK
 });
 requirejs(['main'], function ($) {
-    console.log($) // OK
-});
-requirejs(['displayManager'], function ($) {
-    console.log($) // OK
-});
-requirejs(['aboutBoxContent'], function ($) {
     console.log($) // OK
 });

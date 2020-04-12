2e86e264e72637a668f7cbc4663b3ad910117806
Martha <martha@themiltons.co.uk>
2020-04-12 21:06:27 +0100

    updated the php file to delete data if more than 100 are in the database

--- Php/receiveNewSensorData.php
+++ Php/receiveNewSensorData.php
@@ -37,5 +37,34 @@ include('Validation.php');
          }
          $GLOBALS['dblink']->close();
          $stmt->close();
-     }
+         $deleteSensorData = new deleteSensorData($token);     
+    }
  };
+
+ class deleteSensorData {
+    function __construct($token) {
+        $stmt = $GLOBALS['dblink']->prepare("SELECT COUNT(*) FROM tblsensordata WHERE sensorID = ?");
+        $stmt->bind_param("i", $token,);
+        if ($stmt->execute() === TRUE) {
+            $result = mysqli_stmt_get_result($stmt);
+            if($result > 100){
+                $rowsToDelete = $result - 100;
+                $stmt2 = $GLOBALS['dblink']->prepare("DELETE FROM tblsensordata WHERE sensorID=? ORDER BY dateTime ASC LIMIT ? ");
+                $stmt2->bind_param("ii", $token, $rowsToDelete);
+                if ($stmt2->execute() === TRUE) {
+                    echo "successfully deleted";
+                } else {
+                    echo "database failed to delete";
+                }
+            }
+
+            } else {
+                echo "database up to date";
+            }
+        } else {
+            echo "data entry fail " . "Error: " . $stmt . "<br>" . $GLOBALS['dblink']->error;
+        }
+        $GLOBALS['dblink']->close();
+        $stmt->close();
+        $stmt2->close();
+    }
